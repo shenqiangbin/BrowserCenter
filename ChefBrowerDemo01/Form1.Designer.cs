7ff94368c56cd18024f969f4ba49dd29c47bc51e
win10 <win10@DESKTOP-1OMAAB3>
2018-01-13 22:51:24 +0800

    基本的ChefBrower的使用

--- ChefBrowerDemo01/Form1.Designer.cs
+++ ChefBrowerDemo01/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(742, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(750, 2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 596);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGo;
     }
 }
 
--- ChefBrowerDemo01/Form1.cs
+++ ChefBrowerDemo01/Form1.cs
@@ -6,14 +6,44 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace ChefBrowerDemo01
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser browser;
+
         public Form1()
         {
             InitializeComponent();
+
+            InitBrowser();
+        }
+
+        private void InitBrowser()
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            browser = new ChromiumWebBrowser("http://www.baidu.com");
+
+            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right | AnchorStyles.Top)));
+            this.browser.Location = new System.Drawing.Point(3, 30);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "webBrowser1";
+            this.browser.Size = new System.Drawing.Size(832, 563);
+            this.browser.TabIndex = 0;
+
+            this.Controls.Add(this.browser);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string url = this.textBox1.Text;
+            this.browser.Load(url);
         }
     }
 }
